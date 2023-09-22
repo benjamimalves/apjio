6aa1e56b0e4920f817d9d86948f0274f1c714a75
Benjamim Alves <balves1@pagerduty.com>
2023-09-22 21:58:34 +0100

    Changed socio meta attrs

--- pages/socio.tsx
+++ pages/socio.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import classNames from "classnames";
 import { makeStyles } from "@material-ui/core/styles";
+import Head from "next/head";
 import GridContainer from "../components/Grid/GridContainer";
 import GridItem from "../components/Grid/GridItem";
 import Parallax from "../components/Parallax/Parallax";
@@ -13,9 +14,27 @@ const useStyles = makeStyles(styles);
 
 export default function Donativo() {
   const classes = useStyles();
+  const frontMatter = {
+    title: "Torne-se sócio da APJIO",
+    og_url: "https://www.apjio.pt/socio",
+    og_description: "Ser associado é estar informado sobre assuntos relacionados com a escola, ser ativamente responsável pela educação do seu educando, e poder contribuir para a melhoria das condições de ensino.",
+    og_image_url: "https://www.apjio.pt/img/hero/socios.jpg"
+  }
 
   return (
     <div>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <title>{`APJIO | ${frontMatter.title}`}</title>
+        <meta property="og:site_name" content={`APJIO | ${frontMatter.title}`} />
+        <meta property="og:url" content={frontMatter.og_url} />
+        <meta property="og:description" content={frontMatter.og_description} />
+        <meta property="og:type" content="article" />
+        <meta property="og:image" content={frontMatter.og_image_url} />
+      </Head>
       <Parallax small filter image="/img/hero/socios.jpg">
         <div className={classes.container}>
           <GridContainer>
